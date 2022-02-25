254092b6fad6fe6d683e63bc517c6e30efdc2ab2
Michelle-2104 <chidowemichelle@gmail.com>
2022-02-25 20:11:37 +0200

    Edit main.js index.html style.css

--- main.js
+++ main.js
@@ -1,49 +1,39 @@
-const container = document.querySelector("div")
+const container = document.querySelector("#container")
 
 
-function makeGrid(rows, columns) {
-    while (document.querySelector('button') !== null) {
-        document.querySelector('button').remove()
-    }
+function makeRows(rows, columns) {
+   
     container.style.setProperty("--grid-rows", rows)
     container.style.setProperty("--grid-columns", columns)
-    container.style.width = '960px'
-    container.style.overflow = 'hidden'
    
     for (i = 0; i < (rows * columns); i++) {
-        let square = document.createElement('div')
+        let square = document.createElement("div")
        // square.textContent = (i + 1)// labels squares//
-       square.style.minHeight = '0'
-       square.style.minWidth = '0'
-       square.style.overflow = 'hidden'
-        container.appendChild(square).className = 'grid-item'
-        square.addEventListener('mouseover', () => {
+      
+        container.appendChild(square).className = "grid-item"
+        square.addEventListener("mouseover", () => {
             square.style.backgroundColor = 'black'
-            console.log(square)
+         //  console.log(square) 
         })
     }
-createButton()
-function createButton() {
-    const buttonDiv = document.querySelector('#buttonDiv')
-    const resetButton = document.createElement('button')
-    resetButton.textContent = 'Reset grid!'
-    resetButton.style.margin = '20px'
-    buttonDiv.appendChild(resetButton)
+}
 
-    resetButton.addEventListener('click', () =>
-      document.querySelectorAll('.gridItem').forEach(e => e.remove())
-      let userGridInput = prompt('Please enter the number of grid squares per side (Max: 100): ')
-       if (userGridInput > 100) {
-        alert('ERROR! You specified a grid size larger than the max of 100.')
-         return
+const resetButton = document.createElement("button")
+constbuttonDiv = document.querySelector("buttonDiv")
 
-    } 
+resetButton.textContent = "Reset"
+buttonDiv.append(resetButton)
+resetButton.addEventListener("click", () => {
+    document.querySelectorAll(".grid-item").forEach(e => e.remove())
+    let userGridInput = prompt("Please enter the number of grid squares per side (max:100):")
+    if (userGridInput >100) {
+        alert("ERROR: You specified a grid size larger than the max of 100.")
+        return
+    }
     rows = userGridInput
     columns = userGridInput
-    makeGrid(rows, columns)
+    makeRows(rows, columns)
 })
+  
 
-
-
-}
-makeGrid(16, 16)
+makeRows(16, 16)
